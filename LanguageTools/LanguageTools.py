52bad6399f6a0f51d368c549f8bc3834d0d20bed
Andras Lasso <lasso@queensu.ca>
2022-01-26 12:42:49 -0500

    ENH: First functional version
    Translations can be retrieved from zip files downloaded from crowdin or from the SlicerLanguageTranslations repository.
    Files are unzipped and installed in the application's translation folder.

--- LanguageTools/LanguageTools.py
+++ LanguageTools/LanguageTools.py
@@ -28,58 +28,6 @@ See more information in the <a href="https://github.com/Slicer/SlicerLanguagePac
 Developed of this module was partially funded by <a href="https://chanzuckerberg.com/eoss/proposals/3d-slicer-in-my-language-internationalization-and-usability-improvements/">CZI EOSS grant</a>.
 """
 
-    # Additional initialization step after application startup is complete
-    slicer.app.connect("startupCompleted()", registerSampleData)
-
-#
-# Register sample data sets in Sample Data module
-#
-
-def registerSampleData():
-  """
-  Add data sets to Sample Data module.
-  """
-  # It is always recommended to provide sample data for users to make it easy to try the module,
-  # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
-
-  import SampleData
-  iconsPath = os.path.join(os.path.dirname(__file__), 'Resources/Icons')
-
-  # To ensure that the source code repository remains small (can be downloaded and installed quickly)
-  # it is recommended to store data sets that are larger than a few MB in a Github release.
-
-  # LanguageTools1
-  SampleData.SampleDataLogic.registerCustomSampleDataSource(
-    # Category and sample name displayed in Sample Data module
-    category='LanguageTools',
-    sampleName='LanguageTools1',
-    # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
-    # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
-    thumbnailFileName=os.path.join(iconsPath, 'LanguageTools1.png'),
-    # Download URL and target file name
-    uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-    fileNames='LanguageTools1.nrrd',
-    # Checksum to ensure file integrity. Can be computed by this command:
-    #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
-    checksums = 'SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95',
-    # This node name will be used when the data set is loaded
-    nodeNames='LanguageTools1'
-  )
-
-  # LanguageTools2
-  SampleData.SampleDataLogic.registerCustomSampleDataSource(
-    # Category and sample name displayed in Sample Data module
-    category='LanguageTools',
-    sampleName='LanguageTools2',
-    thumbnailFileName=os.path.join(iconsPath, 'LanguageTools2.png'),
-    # Download URL and target file name
-    uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-    fileNames='LanguageTools2.nrrd',
-    checksums = 'SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97',
-    # This node name will be used when the data set is loaded
-    nodeNames='LanguageTools2'
-  )
-
 #
 # LanguageToolsWidget
 #
@@ -119,26 +67,21 @@ class LanguageToolsWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     # Create logic class. Logic implements all computations that should be possible to run
     # in batch mode, without a graphical user interface.
     self.logic = LanguageToolsLogic()
+    self.logic.logCallback = self.log
 
     # Connections
 
-    # These connections ensure that we update parameter node when scene is closed
-    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
-    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
-
     # These connections ensure that whenever user changes some settings on the GUI, that is saved in the MRML scene
     # (in the selected parameter node).
-    self.ui.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
-    self.ui.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
-    self.ui.imageThresholdSliderWidget.connect("valueChanged(double)", self.updateParameterNodeFromGUI)
-    self.ui.invertOutputCheckBox.connect("toggled(bool)", self.updateParameterNodeFromGUI)
-    self.ui.invertedOutputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
+    self.ui.githubSourceRadioButton.connect("toggled(bool)", lambda toggled, source="github": self.setTranslationSource(source, toggled))
+    self.ui.crowdinSourceRadioButton.connect("toggled(bool)", lambda toggled, source="crowdin": self.setTranslationSource(source, toggled))
 
     # Buttons
-    self.ui.applyButton.connect('clicked(bool)', self.onApplyButton)
+    self.ui.updateButton.connect('clicked(bool)', self.onUpdateButton)
+    self.ui.restartButton.connect('clicked(bool)', self.onRestartButton)
 
     # Make sure parameter node is initialized (needed for module reload)
-    self.initializeParameterNode()
+    self.updateGUIFromSettings()
 
   def cleanup(self):
     """
@@ -160,121 +103,73 @@ class LanguageToolsWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     # Do not react to parameter node changes (GUI wlil be updated when the user enters into the module)
     self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
 
-  def onSceneStartClose(self, caller, event):
-    """
-    Called just before the scene is closed.
-    """
-    # Parameter node will be reset, do not use it anymore
-    self.setParameterNode(None)
-
-  def onSceneEndClose(self, caller, event):
-    """
-    Called just after the scene is closed.
-    """
-    # If this module is shown while the scene is closed then recreate a new parameter node immediately
-    if self.parent.isEntered:
-      self.initializeParameterNode()
-
-  def initializeParameterNode(self):
-    """
-    Ensure parameter node exists and observed.
-    """
-    # Parameter node stores all user choices in parameter values, node selections, etc.
-    # so that when the scene is saved and reloaded, these settings are restored.
-
-    self.setParameterNode(self.logic.getParameterNode())
-
-    # Select default input nodes if nothing is selected yet to save a few clicks for the user
-    if not self._parameterNode.GetNodeReference("InputVolume"):
-      firstVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
-      if firstVolumeNode:
-        self._parameterNode.SetNodeReferenceID("InputVolume", firstVolumeNode.GetID())
-
-  def setParameterNode(self, inputParameterNode):
-    """
-    Set and observe parameter node.
-    Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
-    """
-
-    if inputParameterNode:
-      self.logic.setDefaultParameters(inputParameterNode)
-
-    # Unobserve previously selected parameter node and add an observer to the newly selected.
-    # Changes of parameter node are observed so that whenever parameters are changed by a script or any other module
-    # those are reflected immediately in the GUI.
-    if self._parameterNode is not None:
-      self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
-    self._parameterNode = inputParameterNode
-    if self._parameterNode is not None:
-      self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
-
-    # Initial GUI update
-    self.updateGUIFromParameterNode()
-
-  def updateGUIFromParameterNode(self, caller=None, event=None):
-    """
-    This method is called whenever parameter node is changed.
-    The module GUI is updated to show the current state of the parameter node.
-    """
-
-    if self._parameterNode is None or self._updatingGUIFromParameterNode:
+  def setTranslationSource(self, translationSource, toggled=True):
+    if not toggled:
+      # ignore when radiobutton is untoggled, we just process the toggled event
       return
+    self.ui.gitRepositoryLabel.enabled = (translationSource == "github")
+    self.ui.githubRepositoryEdit.enabled = (translationSource == "github")
+    self.ui.crowdinZipFilePathLabel.enabled = (translationSource == "crowdin")
+    self.ui.crowdinZipFilePathLineEdit.enabled = (translationSource == "crowdin")
 
-    # Make sure GUI changes do not call updateParameterNodeFromGUI (it could cause infinite loop)
-    self._updatingGUIFromParameterNode = True
+  def updateGUIFromSettings(self):
+    settings = slicer.app.userSettings()
+    try:
+      settings.beginGroup("Internationalization/LanguageTools")
+      translationSource = settings.value("TranslationSource", "crowdin")
+      self.ui.crowdinSourceRadioButton.checked = (translationSource == "crowdin")
+      self.ui.githubSourceRadioButton.checked = (translationSource == "github")
+      self.setTranslationSource(translationSource)
+      self.ui.githubRepositoryEdit.text = settings.value("GitRepository", "https://github.com/Slicer/SlicerLanguageTranslations")
+      self.ui.slicerVersionEdit.text = settings.value("SlicerVersion", "master")
+      self.ui.crowdinZipFilePathLineEdit.currentPath = settings.value("CrowdinZipFilePath", "")
+      self.ui.lreleasePathLineEdit.currentPath = settings.value("LreleaseFilePath", "")
+    finally:
+      settings.endGroup()
 
-    # Update node selectors and sliders
-    self.ui.inputSelector.setCurrentNode(self._parameterNode.GetNodeReference("InputVolume"))
-    self.ui.outputSelector.setCurrentNode(self._parameterNode.GetNodeReference("OutputVolume"))
-    self.ui.invertedOutputSelector.setCurrentNode(self._parameterNode.GetNodeReference("OutputVolumeInverse"))
-    self.ui.imageThresholdSliderWidget.value = float(self._parameterNode.GetParameter("Threshold"))
-    self.ui.invertOutputCheckBox.checked = (self._parameterNode.GetParameter("Invert") == "true")
+    if not os.path.exists(self.ui.lreleasePathLineEdit.currentPath):
+      self.ui.settingsCollapsibleButton.collapsed = False
 
-    # Update buttons states and tooltips
-    if self._parameterNode.GetNodeReference("InputVolume") and self._parameterNode.GetNodeReference("OutputVolume"):
-      self.ui.applyButton.toolTip = "Compute output volume"
-      self.ui.applyButton.enabled = True
-    else:
-      self.ui.applyButton.toolTip = "Select input and output volume nodes"
-      self.ui.applyButton.enabled = False
+  def updateSettingsFromGUI(self):
+    settings = slicer.app.userSettings()
+    try:
+      settings.beginGroup("Internationalization/LanguageTools")
+      settings.setValue("TranslationSource", "crowdin" if self.ui.crowdinSourceRadioButton.checked else "github")
+      settings.setValue("GithubRepository", self.ui.githubRepositoryEdit.text)
+      settings.setValue("SlicerVersion", self.ui.slicerVersionEdit.text)
+      settings.setValue("CrowdinZipFilePath", self.ui.crowdinZipFilePathLineEdit.currentPath)
+      settings.setValue("LreleaseFilePath", self.ui.lreleasePathLineEdit.currentPath)
+    finally:
+      settings.endGroup()
 
-    # All the GUI updates are done
-    self._updatingGUIFromParameterNode = False
+    self.ui.crowdinZipFilePathLineEdit.addCurrentPathToHistory()
 
-  def updateParameterNodeFromGUI(self, caller=None, event=None):
-    """
-    This method is called when the user makes any change in the GUI.
-    The changes are saved into the parameter node (so that they are restored when the scene is saved and loaded).
-    """
-
-    if self._parameterNode is None or self._updatingGUIFromParameterNode:
-      return
-
-    wasModified = self._parameterNode.StartModify()  # Modify all properties in a single batch
-
-    self._parameterNode.SetNodeReferenceID("InputVolume", self.ui.inputSelector.currentNodeID)
-    self._parameterNode.SetNodeReferenceID("OutputVolume", self.ui.outputSelector.currentNodeID)
-    self._parameterNode.SetParameter("Threshold", str(self.ui.imageThresholdSliderWidget.value))
-    self._parameterNode.SetParameter("Invert", "true" if self.ui.invertOutputCheckBox.checked else "false")
-    self._parameterNode.SetNodeReferenceID("OutputVolumeInverse", self.ui.invertedOutputSelector.currentNodeID)
-
-    self._parameterNode.EndModify(wasModified)
-
-  def onApplyButton(self):
+  def onUpdateButton(self):
     """
     Run processing when user clicks "Apply" button.
     """
-    with slicer.util.tryWithErrorDisplay("Failed to compute results.", waitCursor=True):
+    with slicer.util.tryWithErrorDisplay("Update failed.", waitCursor=True):
+      self.ui.statusTextEdit.clear()
+      self.updateSettingsFromGUI()
 
-      # Compute output
-      self.logic.process(self.ui.inputSelector.currentNode(), self.ui.outputSelector.currentNode(),
-        self.ui.imageThresholdSliderWidget.value, self.ui.invertOutputCheckBox.checked)
+      self.logic.slicerVersion = self.ui.slicerVersionEdit.text
+      self.logic.lreleasePath = self.ui.lreleasePathLineEdit.currentPath
 
-      # Compute inverted output (if needed)
-      if self.ui.invertedOutputSelector.currentNode():
-        # If additional output volume is selected then result with inverted threshold is written there
-        self.logic.process(self.ui.inputSelector.currentNode(), self.ui.invertedOutputSelector.currentNode(),
-          self.ui.imageThresholdSliderWidget.value, not self.ui.invertOutputCheckBox.checked, showResult=False)
+      self.logic.removeTemporaryFolder()
+
+      if self.ui.crowdinSourceRadioButton.checked:
+        self.logic.unpackTsFilesFromCrowdinZipFile(self.ui.crowdinZipFilePathLineEdit.currentPath)
+      else:
+        self.logic.downloadTsFilesFromGithub(self.ui.githubRepositoryEdit.text)
+
+      self.logic.convertTsFilesToQmFiles()
+      self.logic.installQmFiles()
+
+  def onRestartButton(self):
+    slicer.util.restart()
+
+  def log(self, message):
+    self.ui.statusTextEdit.append(message)
 
 #
 # LanguageToolsLogic
@@ -295,47 +190,96 @@ class LanguageToolsLogic(ScriptedLoadableModuleLogic):
     Called when the logic class is instantiated. Can be used for initializing member variables.
     """
     ScriptedLoadableModuleLogic.__init__(self)
+    self.slicerVersion = "master"
+    self.lreleasePath = None
+    self._temporaryFolder = None
+    self.translationFilesFolder = None
+    self.crowdinProjectName = "Slicer"
+    self.gitRepositoryName = "SlicerLanguageTranslations"
+    self.gitBranchName = "main"  # we store translations for all Slicer versions in the main branch
+    self.logCallback = None
 
-  def setDefaultParameters(self, parameterNode):
+  def log(self, message):
+    if self.logCallback:
+      self.logCallback(message)
+
+  def temporaryFolder(self):
+    if not self._temporaryFolder:
+      self._temporaryFolder = slicer.util.tempDirectory()
+    return self._temporaryFolder
+
+  def removeTemporaryFolder(self):
+    if not self._temporaryFolder:
+      return
+    import shutil
+    shutil.rmtree(self._temporaryFolder)
+    self._temporaryFolder = None
+    self.translationFilesFolder = None
+
+  def unpackTsFilesFromCrowdinZipFile(self, crowdinZipFile):
+    """Extract .ts files from a zip file downloaded from Crowdin.
+    This method requires a temporary folder that does not contain previous downloaded or extracted files.
     """
-    Initialize parameter node with default settings.
-    """
-    if not parameterNode.GetParameter("Threshold"):
-      parameterNode.SetParameter("Threshold", "100.0")
-    if not parameterNode.GetParameter("Invert"):
-      parameterNode.SetParameter("Invert", "false")
 
-  def process(self, inputVolume, outputVolume, imageThreshold, invert=False, showResult=True):
-    """
-    Run the processing algorithm.
-    Can be used without GUI widget.
-    :param inputVolume: volume to be thresholded
-    :param outputVolume: thresholding result
-    :param imageThreshold: values above/below this threshold will be set to 0
-    :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
-    :param showResult: show output volume in slice viewers
+    tempFolder = self.temporaryFolder()
+
+    # Unzip file
+    slicer.util.extractArchive(crowdinZipFile, tempFolder)
+
+    # /temp/[Slicer.SlicerLanguageTranslations] main/translated/[Slicer.SlicerLanguageTranslations] main/master"
+    self.translationFilesFolder = f'{tempFolder}/[{self.crowdinProjectName}.{self.gitRepositoryName}] {self.gitBranchName}/translated/[{self.crowdinProjectName}.{self.gitRepositoryName}] {self.gitBranchName}/{self.slicerVersion}'
+
+  def downloadTsFilesFromGithub(self, githubRepositoryUrl):
+    """Download .ts files from a Github repository.
+    This method requires a temporary folder that does not contain previous downloaded or extracted files.
     """
 
-    if not inputVolume or not outputVolume:
-      raise ValueError("Input or output volume is invalid")
+    tempFolder = self.temporaryFolder()
 
-    import time
-    startTime = time.time()
-    logging.info('Processing started')
+    # Download file
+    import SampleData
+    dataLogic = SampleData.SampleDataLogic()
+    translationZipFilePath = dataLogic.downloadFile(f'{githubRepositoryUrl}/archive/refs/heads/{self.gitBranchName}.zip', self.temporaryFolder(), 'CrowdinTranslations.zip')
 
-    # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
-    cliParams = {
-      'InputVolume': inputVolume.GetID(),
-      'OutputVolume': outputVolume.GetID(),
-      'ThresholdValue' : imageThreshold,
-      'ThresholdType' : 'Above' if invert else 'Below'
-      }
-    cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
-    # We don't need the CLI module node anymore, remove it to not clutter the scene with it
-    slicer.mrmlScene.RemoveNode(cliNode)
+    # Unzip file
+    slicer.util.extractArchive(translationZipFilePath, tempFolder)
 
-    stopTime = time.time()
-    logging.info(f'Processing completed in {stopTime-startTime:.2f} seconds')
+    # /temp.../SlicerLanguageTranslations-main/translated/master
+    self.translationFilesFolder = f'{tempFolder}/{self.gitRepositoryName}-{self.gitBranchName}/translated/{self.slicerVersion}'
+
+  def convertTsFilesToQmFiles(self):
+    if not self.translationFilesFolder:
+      raise ValueError("Translation files folder is not specified.")
+
+    if not os.path.exists(self.lreleasePath):
+      raise ValueError("lrelease tool path is not specified.")
+
+    from pathlib import Path
+    tsFiles = Path(self.translationFilesFolder).glob('*.ts')
+    for file in tsFiles:
+        lreleaseProcess = slicer.util.launchConsoleProcess([self.lreleasePath, str(file)])
+        slicer.util.logProcessOutput(lreleaseProcess)
+
+  def installQmFiles(self):
+    if not self.translationFilesFolder:
+      raise ValueError("Translation files folder is not specified.")
+
+    import shutil
+    from pathlib import Path
+    tsFiles = Path(self.translationFilesFolder).glob('*.qm')
+
+    applicationTranslationFolder = slicer.app.translationFolders()[0]
+
+    numberOfInstalledFiles = 0
+    for file in tsFiles:
+      logging.debug(f"Installing translation file: {file} in {applicationTranslationFolder}")
+      shutil.copy(file, applicationTranslationFolder)
+      numberOfInstalledFiles += 1
+
+    if numberOfInstalledFiles == 0:
+      raise ValueError(f"No translation (qm) files were found at {self.translationFilesFolder}")
+    
+    self.log(f"Installed {numberOfInstalledFiles} translation files in {applicationTranslationFolder}")
 
 #
 # LanguageToolsTest
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SlicerLanguagePacks
-3D Slicer extension for creating, editing, and storing translations for Slicer core and extensions
+
+3D Slicer extension for creating, editing, and storing translations for Slicer core and extensions.
